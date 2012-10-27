dec72c2404d18c868dd2ed548204726c70ebdc3a
HVN <hvnsweeting@gmail.com>
2012-10-27 10:52:29 +0700

    update

--- flush_endpoint.sh
+++ flush_endpoint.sh
@@ -2,7 +2,7 @@
 
 # delete all endpoints
 . rc
-alias ks="keystone --token $TOKEN --endpoint $ENDURL"
+
 IDS=$(keystone --token $TOKEN --endpoint $ENDURL endpoint-list | cut -d"|" -f2 | tail -n+4 | head -n-1 | xargs)
 for id in $IDS; do
     keystone --token $TOKEN --endpoint $ENDURL endpoint-delete $id
