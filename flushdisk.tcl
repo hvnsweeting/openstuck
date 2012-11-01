cbfe48fbbaf8d9caaa162b37f0ad5346d0be1842
hvn <hvn@ubunta.(none)>
2012-11-01 16:37:59 +0700

    update

--- flushdisk.tcl
+++ flushdisk.tcl
@@ -1,5 +1,6 @@
 #!/usr/bin/env tclsh
 # expect must be installed
+# I used tcl here because "yes | glance delete $ID not work"
 package require Expect
 
 set ids [exec glance index | tail -n +3 | sed -n {s/ .*//gp}]
